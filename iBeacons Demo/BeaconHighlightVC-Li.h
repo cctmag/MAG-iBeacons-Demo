8e2fb5a493b68e1502ea5860e39fdc4c1e543cba
dsholmes <holmes.danny@gmail.com>
2014-03-21 16:45:07 -0500

    Made iPad Storyboard
    and started a third view controller

---
+++ iBeacons Demo/BeaconHighlightVC-Li.h
@@ -0,0 +1,13 @@
+//
+//  BeaconHighlightVC-Li.h
+//  iBeacons Demo
+//
+//  Created by Danny Holmes on 3/21/14.
+//  Copyright (c) 2014 Mobient. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface BeaconHighlightVC_Li : ViewController
+
+@end
