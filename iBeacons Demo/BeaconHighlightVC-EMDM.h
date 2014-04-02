f2416311a76fa48c897e298dc4bb1dd0b53091dd
Jesse Allison <jesse@electrotap.com>
2014-04-02 12:28:23 -0500

    Merge branch 'EMDM-Highlight'
    Conflicts:
    iBeacons Demo.xcodeproj/project.pbxproj
    iBeacons Demo/Main-iPad.storyboard
    iBeacons Demo/Main.storyboard
    iBeacons Demo/ViewController.m

---
+++ iBeacons Demo/BeaconHighlightVC-EMDM.h
@@ -0,0 +1,13 @@
+//
+//  BeaconHighlightVC-EMDM.h
+//  iBeacons Demo
+//
+//  Created by Jesse Allison on 3/21/14.
+//  Copyright (c) 2014 Mobient. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BeaconHighlightVC_EMDM : UIViewController
+
+@end
